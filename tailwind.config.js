66633899a8c50c771cfc35578e2bccb596e30bbb
timocare <timocare@gmail.com>
2022-02-10 13:14:42 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+//tailwind.config.js
+module.exports = {
+    purge: [
+            // use .tsx if using TypeScript
+            './pages/**/*.js',
+            './components/**/*.js'
+
+        ]
+        //..
+}
